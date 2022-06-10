495c2787cc32c363b2bb542b95610c00ab231882
MctomSpdo <mctom.spdo@gmail.com>
2022-06-10 09:28:15 +0200

    fix init sql

--- docker/postgres-db/init.sql
+++ docker/postgres-db/init.sql
@@ -1,63 +1,54 @@
-CREATE TABLE player
-(
-    PlayerId serial PRIMARY KEY,
-    name     varchar(50) unique not null
+CREATE TABLE player (
+                        PlayerId serial PRIMARY KEY,
+                        name varchar(50) unique not null
 );
 
-CREATE TABLE game
-(
-    GameId   serial PRIMARY KEY,
-    PlayerId int not null,
-    FOREIGN KEY (PlayerId) REFERENCES player (PlayerId)
+CREATE TABLE game (
+                      GameId serial PRIMARY KEY,
+                      PlayerId int not null,
+                      FOREIGN KEY (PlayerId) REFERENCES player (PlayerId)
 );
 
-CREATE TABLE question
-(
-    QuestionId int PRIMARY KEY,
-    question   varchar(256) not null,
-    difficulty int
+CREATE TABLE question (
+                          QuestionId int PRIMARY KEY,
+                          question varchar(256) not null,
+                          difficulty int
 );
 
-CREATE TABLE answer
-(
-    AnswerId   int PRIMARY KEY,
-    answer     varchar(256) not null,
-    iscorrect  bool         not null,
-    worth      int          not null,
-    QuestionId int,
-    FOREIGN KEY (QuestionId) REFERENCES question (QuestionId)
+CREATE TABLE answer (
+                        AnswerId int PRIMARY KEY,
+                        answer varchar(256) not null,
+                        iscorrect bool not null,
+                        worth int not null,
+                        QuestionId int,
+                        FOREIGN KEY (QuestionId) REFERENCES question (QuestionId)
 );
 
-CREATE TABLE game_question
-(
-    GameQuestionId int not null PRIMARY KEY,
-    GameId         int not null,
-    QuestionId     int not null,
-    answeredFrom   int not null,
-    FOREIGN KEY (answeredFrom) references player (PlayerId),
-    FOREIGN KEY (GameId) REFERENCES game (GameId),
-    FOREIGN KEY (QuestionId) REFERENCES question (QuestionId)
+CREATE TABLE game_question (
+                               GameQuestionId serial not null PRIMARY KEY,
+                               GameId int not null,
+                               QuestionId int not null,
+                               answeredFrom int not null,
+                               FOREIGN KEY (answeredFrom) references player (PlayerId),
+                               FOREIGN KEY (GameId) REFERENCES game (GameId),
+                               FOREIGN KEY (QuestionId) REFERENCES question(QuestionId)
 );
 
-CREATE TABLE question_answer
-(
-    QuestionId int not null,
-    AnswerId   int not null,
-    PRIMARY KEY (QuestionId, AnswerId),
-    FOREIGN KEY (QuestionId) REFERENCES question (QuestionId),
-    FOREIGN KEY (AnswerId) REFERENCES answer (AnswerId)
+CREATE TABLE question_answer (
+                                 QuestionId int not null,
+                                 AnswerId int not null,
+                                 PRIMARY KEY (QuestionId, AnswerId),
+                                 FOREIGN KEY (QuestionId) REFERENCES question (QuestionId),
+                                 FOREIGN KEY (AnswerId) REFERENCES answer (AnswerId)
 );
 
-CREATE TABLE answered
-(
-    Game_QuestionId int not null,
-    AnswerId        int not null,
-    PRIMARY KEY (Game_QuestionId, AnswerId),
-    FOREIGN KEY (Game_QuestionId) REFERENCES game_question (GameQuestionId),
-    FOREIGN KEY (AnswerId) REFERENCES answer (AnswerId)
+CREATE TABLE answered (
+                          Game_QuestionId int not null,
+                          AnswerId int not null,
+                          PRIMARY KEY (Game_QuestionId, AnswerId),
+                          FOREIGN KEY (Game_QuestionId) REFERENCES game_question (GameQuestionId),
+                          FOREIGN KEY (AnswerId) REFERENCES answer (AnswerId)
 );
-
-/** INSERT **/
 
 INSERT INTO question(questionid, question, difficulty)
 values (1, 'Wie viele Plastikteile schwimmern in den Meeren', 7),
